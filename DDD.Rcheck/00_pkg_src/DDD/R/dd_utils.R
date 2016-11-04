803d0d34166821614b102c4d14e9f3896a9c442c
rsetienne <rampaletienne@gmail.com>
2016-11-04 16:57:41 +0100

    conversion bug fix in L2phylo

--- DDD.Rcheck/00_pkg_src/DDD/R/dd_utils.R
+++ DDD.Rcheck/00_pkg_src/DDD/R/dd_utils.R
@@ -119,9 +119,7 @@ L2phylo = function(L,dropextinct = T)
       tend = (L[,4] == -1) * age + (L[,4] > -1) * L[,4]
    }
    L = L[,-4]
-   linlist = cbind(data.frame(L[sall,]),paste("t",abs(L[sall,3]),sep = ""),tend)
-   linlist[,4] = as.character(linlist[,4])
-   names(linlist) = 1:5
+   linlist = cbind(L[sall,],paste("t",abs(L[sall,3]),sep = ""),tend)
    done = 0
    while(done == 0)
    {
@@ -132,19 +130,19 @@ L2phylo = function(L,dropextinct = T)
       parentinlist = length(parentj)
       if(parentinlist == 1)
       {
-         spec1 = paste(linlist[parentj,4],":",linlist[parentj,5] - linlist[j,1],sep = "")
-         spec2 = paste(linlist[j,4],":",linlist[j,5] - linlist[j,1],sep = "")
+         spec1 = paste(linlist[parentj,4],":",as.numeric(linlist[parentj,5]) - as.numeric(linlist[j,1]),sep = "")
+         spec2 = paste(linlist[j,4],":",as.numeric(linlist[j,5]) - as.numeric(linlist[j,1]),sep = "")
          linlist[parentj,4] = paste("(",spec1,",",spec2,")",sep = "")
          linlist[parentj,5] = linlist[j,1]
          linlist = linlist[-j,]
       } else {
          #linlist[j,1:3] = L[abs(as.numeric(parent)),1:3]
-         linlist[j,1:3] = L[which(L[,3] == parent),1:3]
+         linlist[j,1:3] = L[which(L[,3] == as.numeric(parent)),1:3]
       }
-      if(nrow(linlist) == 1) { done = 1 }
+      if(is.null(nrow(linlist))) { done = 1 }
    }
    linlist[4] = paste(linlist[4],":",linlist[5],";",sep = "")
-   phy = read.tree(text = linlist[1,4])
+   phy = read.tree(text = linlist[4])
    tree = as.phylo(phy)
    return(tree)
 }
